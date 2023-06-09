761ac0ed245288146f756fd79df92f297dd6c402
SavaOurl <dendiroblek@gmail.com>
2023-06-09 19:11:53 +0400

    added the ourcare

--- src/components/pages/MainPage/MainPage.js
+++ src/components/pages/MainPage/MainPage.js
@@ -35,12 +35,49 @@ const MainPage = () => {
                 <div className="MainPage__welcome_picture-small"></div>
                 <div className="MainPage__welcome_picture-leaf"></div>
                 <div className="MainPage__welcome-wrapper">
-                    <div className="MainPage__welcome_title-14px">welcome</div>
-                    <div className="MainPage__welcome_title-48px">Hi, meet TreeHouse.</div>
-                    <div className="MainPage__welcome_describe">
+                    <div className="MainPage__title-14px">welcome</div>
+                    <div className="MainPage__title-48px">Hi, meet TreeHouse.</div>
+                    <div className="MainPage__describe">
                         Mindfully crafted gender-neutral children's clothing focused on your little one and designed with great care for our planet and the people on it.
                     </div>
                     <Link to='journal' className="MainPage__btn MainPage__btn-green">Read more</Link>
+                </div>
+            </div>
+            <div className="MainPage__ourcare">
+                <div className="MainPage__ourcare_info-left">
+                    <div className="MainPage__title-14px">Our care</div>
+                    <div className="MainPage__title-48px">
+                        We prioritize your peace of mind.
+                    </div>
+                    <div className="MainPage__ourcare_text-wrapper">
+                        <h2 className="MainPage__ourcare_text-title"><span>1</span>  saving your time</h2>
+                        <div className="MainPage__describe">
+                            Each item in our collection seamlessly matches the next. Invest in a few pieces or the entire line for a season's worth of clothing options, designed to last and be loved by others once your little one has grown.
+                        </div>
+                    </div>
+                    <div className="MainPage__ourcare_text-wrapper">
+                        <h2 className="MainPage__ourcare_text-title"><span>2</span>  ethical manufacturing</h2>
+                        <div className="MainPage__describe">
+                            Our commitment to ethical production isn’t just rhetoric. We place the welfare of workers and the environment at the forefront of our decision making. Always.
+                        </div>
+                    </div>
+                </div>
+                <div className="MainPage__ourcare_photo-kids"></div>
+                <div className="MainPage__ourcare_photo-hands"></div>
+                <div className="MainPage__ourcare_photo-girl"></div>
+                <div className="MainPage__ourcare_info-right">
+                    <div className="MainPage__ourcare_text-wrapper">
+                        <h2 className="MainPage__ourcare_text-title"><span>3</span>  The highest quality</h2>
+                        <div className="MainPage__describe">
+                            We use only he highest quality sustainable fabrics, including GOTS-certified organic cotton and OEKO-TEX    STANDARD 100 linen, to ensure your child's delicate skin is cared for.
+                        </div>
+                    </div>
+                    <div className="MainPage__ourcare_text-wrapper">
+                        <h2 className="MainPage__ourcare_text-title"><span>4</span>  womAn owned brand</h2>
+                        <div className="MainPage__describe">
+                            When you choose us, you're supporting an ethical and sustainable small business owned by a woman. We don't just talk the talk, we walk the walk.
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
